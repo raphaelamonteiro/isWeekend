c1e1860df21de841b102b4ac3465781470290dfa
Raphaela <125154913+raphaelamonteiro@users.noreply.github.com>
2023-07-14 00:08:19 -0300

    Add files via upload

---
+++ isWeekend.js
@@ -0,0 +1,10 @@
+<script>
+    function isWeekend(date) {
+        //obter dia da semana
+        const dayOfWeek = date.getDay();
+
+    
+        //verifica se é sábado(6) ou domingo(0)
+        return dayOfWeek 
+    }
+</script>
